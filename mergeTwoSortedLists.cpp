2d415315e961a2bb1e21ea9dbc8d0bad0af070ba
Javad Hosseinpour <cavadhosseinpour@gmail.com>
2026-08-28 07:27:37 -0400

    Week 1 - 3. Merge Two Linked List - Added Recursive approach

--- mergeTwoSortedLists.cpp
+++ mergeTwoSortedLists.cpp
@@ -40,9 +40,65 @@ public:
         }
         return dummy.next;
     }
+
+    // Recursive function to merge two sorted linked lists
+    ListNode* mergeTwoLists_recursive(ListNode* list1, ListNode* list2)
+    {
+        // Base Case 1: If the first list is empty, return the second list
+        if (!list1) return list2;
+        // Base Case 2: If the second list is empty, return the first list
+        if (!list2) return list1;
+
+        // Recursive step: Determine which node has the smaller value
+        if (list1->val <= list2->val)
+        {
+            // Link the current node to the result of the next recursive merge
+            list1->next = mergeTwoLists_recursive(list1->next, list2);
+            return list1; // Return the head of the merged list
+        }
+        else
+        {
+            // Link the current node to the result of the next recursive merge
+            list2->next = mergeTwoLists_recursive(list1, list2->next);
+            return list2; // Return the head of the merged list
+        }
+    }
+
+    // Helper function to print the linked list
+    void printList(ListNode* head)
+    {
+        while (head)
+        {
+            std::cout << head->val << " -> ";
+            head = head->next;
+        }
+        std::cout << "nullptr\n";
+    }
 };
 
 int main()
 {
+    Solution sol;
+    // Creating List 1: 1 -> 3 -> 5
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(3);
+    list1->next->next = new ListNode(5);
+
+    // Creating List 2: 2 -> 4 -> 6
+    ListNode* list2 = new ListNode(2);
+    list2->next = new ListNode(4);
+    list2->next->next = new ListNode(6);
+
+    std::cout << "Original List 1: ";
+    sol.printList(list1);
+    std::cout << "Original List 2: ";
+    sol.printList(list2);
+
+    // Merging lists
+    ListNode* mergedHead = sol.mergeTwoLists(list1, list2);
+
+    std::cout << "Merged Sorted List: ";
+    sol.printList(mergedHead);
+
     return 0;
 }
